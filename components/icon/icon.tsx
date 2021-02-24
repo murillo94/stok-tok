1aa7146845e4e092dc3da9b6ede160b99f9b0d4c
Murillo de Miranda Pereira <murillobeta0@hotmail.com>
2021-02-24 14:22:34 -0300

    feat: create icon types props

--- components/icon/icon.tsx
+++ components/icon/icon.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import { Feather } from '@expo/vector-icons';
 
+import colors from 'constants/colors';
+
 import { IconName } from 'typings/icon';
 
-export function Icon(props: { name: IconName; color: string }) {
-  return <Feather size={24} {...props} />;
+type IconProps = {
+  name: IconName;
+  size?: number;
+  color?: string;
+};
+
+export function Icon({
+  name,
+  size = 24,
+  color = colors.light.text,
+  ...props
+}: IconProps) {
+  return <Feather name={name} size={size} color={color} {...props} />;
 }
